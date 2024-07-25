7ca7ba1042c8fb5adec93d1d7a758a768a7e2bb0
DhruvRR005 <dhruvrajsingh005@gmail.com>
2024-07-25 17:56:32 +0530

    Done !!!

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div className='items-center justify-center'>
+        <hr className='border-[2px] border-black' />
+        <h1 className='text-center py-3 text-sm'>
+            Copyright @ 2024 - All right reserved by ACME Industries 
+            Ltd
+        </h1>
+
+    </div>
+    </>
+  )
+}
+
+export default Footer;
